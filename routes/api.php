b06100aa7682af758ccd2cc57838541a029a24f0
Raja Ram <raja.ram@abtach.org>
2020-09-18 10:48:27 +0500

    early route file settings

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix('site')
+    ->group(base_path('routes/apisite.php'));
+
+Route::prefix('cms')
+    ->group(base_path('routes/apicms.php'));    
